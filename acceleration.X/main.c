fe07774f3cdb9b4964f50a3e6fecb2c47162949f
arao <araobp@users.noreply.github.com>
2017-01-19 00:49:29 +0900

    update

--- acceleration.X/main.c
+++ acceleration.X/main.c
@@ -2,12 +2,6 @@
 #include "mcc_generated_files/mcc.h"
 #define _XTAL_FREQ 500000
 
-#define RANGE 2.0
-
-float calc_g(adc_result_t result) {
-    return RANGE * ((float)result - 512.0) / 512.0;
-}
-
 void main(void)
 {
     SYSTEM_Initialize();
@@ -26,26 +20,19 @@ void main(void)
         ADC_StartConversion();
         while(!ADC_IsConversionDone());
         adc_result_t x = ADC_GetConversionResult();
-        float x_g = calc_g(x);
        
         // Y-axis
         ADC_SelectChannel(channel_AN2);
         ADC_StartConversion();
         while(!ADC_IsConversionDone());
         adc_result_t y = ADC_GetConversionResult();
-        float y_g = calc_g(y);
        
-        __delay_ms(100);
         // Z-axis
         ADC_SelectChannel(channel_AN6);
         ADC_StartConversion();
         while(!ADC_IsConversionDone());
-        adc_result_t z = ADC_GetConversionResult();
-        float z_g = calc_g(z);
+        adc_result_t z = ADC_GetConversionResult();        
         
-        
-        //printf("%d\n", x);
-        printf("%f,%f,%f\n", x_g, y_g, z_g);
-
+        printf("%d,%d,%d\n", x, y, z);
     }
 }
