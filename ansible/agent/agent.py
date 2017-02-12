dfe5e0f99590f22ed59adb65cdda8f3ffa80d1b7
arao <araobp@users.noreply.github.com>
2017-02-12 14:29:58 +0000

    read data from real sensors

--- ansible/agent/agent.py
+++ ansible/agent/agent.py
@@ -3,20 +3,41 @@
 import paho.mqtt.client as client
 from time import sleep
 import yaml
+import serial
+import os
+import json
 
-HOST = 'localhost'
-PORT = 1883
-
+# MQTT topic
 TOPIC = 'sensor'
 
-test_msg = 'Hello World!'
-
 if __name__ == '__main__':
+
     f = open('./agent.yaml', 'r')
+
     conf = yaml.load(f)
+    cpe = conf['cpe']
+    location = conf['location']
     mqtt = conf['mqtt']
+
     client = client.Client()
     client.connect(host=mqtt['host'], port=mqtt['port'], keepalive=60)
+
+    # FTDI driver
+    ftdi_list = os.listdir('/dev/serial/by-id')
+
+    dev_list = []
+    for ftdi in ftdi_list:
+        path = '/dev/serial/by-id/{}'.format(ftdi)
+        tty = os.path.realpath(path)  # symbolic link
+        ser = serial.Serial(tty)
+        usb = tty.split('/')[2]
+        dev_list.append((ftdi, usb, ser))
+
     while True:
-        client.publish(TOPIC, test_msg)
-        sleep(1)
+        for dev in dev_list:
+            usb = dev[1]
+            ser = dev[2]
+            raw_data = ser.readline()[:-1]
+            data = json.dumps(dict(cpe=cpe, location=location, usb=usb, data=raw_data))
+            client.publish(TOPIC, data)
+
