34edba727fe3d3a8d561ce72cc4a086ef609f26e
arao <araobp@users.noreply.github.com>
2017-02-18 22:37:18 +0900

    update

--- protocol.X/main.c
+++ protocol.X/main.c
@@ -1,32 +1,44 @@
 #include "mcc_generated_files/mcc.h"
+#include "protocol.h"
 #include <stdlib.h>
+#include <string.h>
+
+#define DEVICE_ID "test0"
 
 void main(void)
 {
     SYSTEM_Initialize();
 
     uint8_t c;
-    uint8_t cnt;
-    uint8_t buf[4];
-    uint8_t byte_value;
+    uint8_t cnt = 0;
+    uint8_t buf[16];
+    uint8_t interval;
+    uint8_t setting;
 
     while (1)
     {
         do {
             c = EUSART_Read();
-            // printf("%c\n", c);
+            buf[cnt++] = c;
             if (c == '\n') {
-                
                 buf[cnt] = '\0';
                 cnt = 0;
-                byte_value = atoi(buf);
-                DATAEE_WriteByte(0, byte_value);
-                // printf("New Period: %s\n", buf);
-            } else if (cnt < 3) {
-                buf[cnt++] = c;
-            } else {
-                cnt = 0;
-                break;
+                
+                if (!strncmp(WHO, buf, 3)) {
+                    printf("%s\n", DEVICE_ID);
+                } else if (!strncmp(INT, buf, 3)) {
+                    interval = atoi(&buf[4]);
+                    DATAEE_WriteByte(0, interval);
+                } else if (!strncmp(SAV, buf ,3)) {
+                    DATAEE_WriteByte(0, setting);
+                } else if (!strncmp(STA, buf, 3)) {
+                    // kick
+                } else if (!strncmp(STP, buf, 3)) {
+                    // kick
+                } else {
+                    setting = atoi(buf);
+                    // kick
+                }
             }
         } while (EUSART_DataReady);
     }
---
+++ protocol.X/protocol.h
@@ -0,0 +1,22 @@
+#ifndef PROTOCOL_H
+#define	PROTOCOL_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+/*
+ * Commands
+ */
+#define WHO "WHO"
+#define INT "INT"
+#define SAV "SAV"
+#define STA "STA"
+#define STP "STP"
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* PROTOCOL_H */
+
