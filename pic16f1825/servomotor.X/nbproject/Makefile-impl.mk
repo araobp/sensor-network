a3a2496ebc0807072b9ad4d08bd4345b5975cf5d
arao <araobp@users.noreply.github.com>
2017-03-19 23:23:33 +0900

    rename

--- pic16f1825/servomotor.X/nbproject/Makefile-impl.mk
+++ pic16f1825/servomotor.X/nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=servomotor
+PROJECTNAME=servomotor.X
 
 # Active Configuration
 DEFAULTCONF=default
--- pic16f1825/servomotor.X/nbproject/Package-default.bash
+++ pic16f1825/servomotor.X/nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/servomotor.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=servomotor.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=servomotor/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/servomotor.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=servomotor.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=servomotor.x/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/servomotor/bin
+makeDirectory ${TMPDIR}/servomotor.x/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/servomotor.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/servomotor.x.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/servomotor.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/servomotor.x.tar *
 checkReturnCode
 
 # Cleanup
