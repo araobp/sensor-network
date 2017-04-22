c7aedcb38679e6d3583abe2b9f37476a543cc3db
arao <araobp@users.noreply.github.com>
2017-04-22 16:13:35 +0900

    update

--- pic16f1825/backplane_master.X/main.c
+++ pic16f1825/backplane_master.X/main.c
@@ -7,20 +7,22 @@
 #define _XTAL_FREQ 500000
 #define DEVICE_ID "BACKPLANE_MASTER"
 
-//#define TEST_ADDRESS 0x01
-//#define TEST_REGISTER 0x02
-//#define TEST_DATA 0x03
-
+// Backplane-master-specific commands
 #define I2C "I2C"
 #define MAP "MAP"
+#define STS "STS"
+
+// Debug commands
 #define DEV "DEV"
 #define REG "REG"
 #define RED "RED"
-#define STS "STS"
-#define SEN "SEN"
 #define WRT "WRT"
+#define SEN "SEN"
 
 #define MAX_DEV_ADDR 48  // it must be larger than 16 and multipels of 8
+#define MASK 0x01
+
+const uint8_t MAX_Y = MAX_DEV_ADDR/8 - 1;
 
 bool running = true;
 bool do_func_plg = false;
@@ -28,14 +30,9 @@ bool do_func_sts = false;
 uint8_t timer_cnt_plg = 0;
 uint8_t timer_cnt_sts = 0;
 uint8_t t_sts = T_STS;
-
-void start_handler(void) {
-    running = true;
-}
-
-void stop_handler(void) {
-    running = false;
-}
+uint8_t read_buf[16];
+uint8_t dev_map[MAX_DEV_ADDR/8];  // I2C slave device map
+uint8_t x, y;
 
 void set_handler(uint8_t value) {
     t_sts = value/10;
@@ -51,9 +48,6 @@ void tmr0_handler(void) {
         do_func_sts = true;
      }
  }
-
-uint8_t dev_map[MAX_DEV_ADDR/8];  // I2C slave device map
-uint8_t x, y;
 
 void clear_dev_map(void) {
     for (y=0; y<MAX_DEV_ADDR/8; y++) {
@@ -78,37 +72,78 @@ void del_dev(uint8_t dev_addr) {
 }
 
 void print_dev_map(void) {
-    uint8_t yy = MAX_DEV_ADDR/8 - 1;
-    for (y=0; y<yy; y++) printf("%x,", dev_map[y]);
+    for (y=0; y<MAX_Y; y++) printf("%x,", dev_map[y]);
     printf("%x\n", dev_map[y]);
 }
 
+uint16_t concat(uint8_t msb, uint8_t lsb) {
+    return (uint16_t)msb << 8 & (uint16_t)lsb;    
+}
+
+void print_tlv(uint8_t dev_addr, uint8_t type, uint8_t length, uint8_t *pbuffer) {
+    uint8_t i;
+    uint8_t len;
+    int16_t v;
+    printf("%%%d:", dev_addr);
+    switch(type) {
+        case TYPE_UINT8_T:
+            len = length - 1;
+            for (i=0; i<len; i++) printf("%d,", pbuffer[i]);
+            printf("%d\n", pbuffer[i]);
+            break;
+        case TYPE_INT8_T:
+            len = length - 1;
+            for (i=0; i<len; i++) printf("%d,", (int8_t)pbuffer[i]);
+            printf("%d\n", (int8_t)pbuffer[i]);            
+            break;
+        case TYPE_UINT16_T:
+            len = length - 2;
+            for (i=0; i<len; i++) printf("%d,", concat(pbuffer[i], pbuffer[++i]));
+            printf("%d\n", concat(pbuffer[i], pbuffer[++i]));                        
+            break;
+        case TYPE_INT16_T:
+            len = length - 2;
+            for (i=0; i<len; i++) printf("%d,", (int16_t)(concat(pbuffer[i], pbuffer[++i])));
+            printf("%d\n", (int16_t)(concat(pbuffer[i], pbuffer[++i])));                        
+            break;
+        case TYPE_FLOAT:
+            len = length - 2;
+            for (i=0; i<len; i++) {
+                v = (int16_t)(concat(pbuffer[i], pbuffer[++i]));
+                printf("%d.%d,", v/100, v%100);
+            }
+            v = (int16_t)(concat(pbuffer[i], pbuffer[++i]));
+            printf("%d.%d,", v/100, v%100);
+            break;
+    }
+}
+
 uint8_t dev_map_iterator() {
-    static uint8_t x = 0;
-    static uint8_t y = 0;
-    static const uint8_t yy = MAX_DEV_ADDR/8 - 1;
+    static uint8_t xx = 0;
+    static uint8_t yy = 0;
     static bool start = true;
-    static const uint8_t mask = 0b00000001;
     bool exist = false;
-    uint8_t x_loc;
     uint8_t dev_addr;
+    uint8_t test;
+    
     do {
-        if (x > 7) {
-            x = 0;
-            ++y;
+        if (xx > 7) {
+            xx = 0;
+            ++yy;
         }
-        if (y >= yy) {
-            x = 0;
-            y = 0;
+        if (yy >= MAX_Y) {
+            xx = 0;
+            yy = 0;
             break;
         }
-        x_loc = mask << x;
-        if (x_loc & dev_map[y]) {
-            dev_addr = y * 8 + x;
+        test = (MASK << xx) & dev_map[yy];
+        if (test) {
+            dev_addr = yy * 8 + xx;
             exist = true;
         }
-        ++x;
+        ++xx;
     } while (!exist);
+    
     if (exist) {
         exist = false;
         return dev_addr;
@@ -120,19 +155,16 @@ uint8_t dev_map_iterator() {
 uint8_t sen(uint8_t dev_addr) {
     uint8_t status;
     uint8_t type;
-    uint8_t read_buf[16];
     uint8_t length ,data, i;
-    status = i2c_read(BACKPLANE_SLAVE_ADDRESS, STS_I2C, &data, 1);
+    status = i2c_read(dev_addr, STS_I2C, &data, 1);
     if (data == STS_SEN_READY) {
-        status = i2c_read(BACKPLANE_SLAVE_ADDRESS, SEN_I2C, &type, 1);
+        status = i2c_read(dev_addr, SEN_I2C, &type, 1);
         if (status == 0) {
-            status = i2c_read(BACKPLANE_SLAVE_ADDRESS, SEN_I2C, &length, 1);
+            status = i2c_read(dev_addr, SEN_I2C, &length, 1);
             if (status == 0) {
-                status = i2c_read(BACKPLANE_SLAVE_ADDRESS, SEN_I2C, &read_buf[0], length);
+                status = i2c_read(dev_addr, SEN_I2C, &read_buf[0], length);
                 if (status == 0) {
-                    printf("%%%d:%d,%d,", dev_addr, type, length);
-                    if (length > 1) for (i=0; i < length - 1; i++) printf("%d,", read_buf[i]);
-                    printf("%d\n", read_buf[i]);
+                    print_tlv(dev_addr, type, length, &read_buf[0]);
                 }
             }                    
         }   
@@ -144,19 +176,14 @@ uint8_t sen(uint8_t dev_addr) {
 
 void loop_func(void) {
     uint8_t dev_addr;
-    uint8_t read_buf[16];
     bool blink_led = false;
     if (do_func_plg) {
-        //printf("general call\n");
         uint8_t status = i2c_write_no_data(GENERAL_CALL_ADDRESS, PLG_I2C);
-        //printf("%d\n", status);
         if (status == 0) {
             for (dev_addr=1; dev_addr<=MAX_DEV_ADDR; dev_addr++) {
                 status = i2c_read(dev_addr, WHO_I2C, &read_buf[0], 1);
                 if (status == 0) {
-                    //printf("%d\n", read_buf[0]);
                     add_dev(read_buf[0]);
-                    //print_dev_map();
                 }
             }
         }
@@ -177,14 +204,15 @@ void loop_func(void) {
 }
 
 void extension_handler(uint8_t *buf) {
-    static uint8_t dev_addr;
-    static uint8_t reg_addr;
     uint8_t data;
     uint8_t type;
     uint8_t length;
-    uint8_t read_buf[16];
     uint8_t i, status;
-        
+
+    /***** For debug commands *****/
+    static uint8_t dev_addr;
+    static uint8_t reg_addr;
+
     if (!strncmp(I2C, buf, 3)) {
         BACKPLANE_SLAVE_ADDRESS = atoi(&buf[4]);
     } else if (!strncmp(WHO, buf, 3)) {
@@ -210,8 +238,8 @@ void extension_handler(uint8_t *buf) {
     } else if (!strncmp(STS, buf, 3)) {
         i2c_read(BACKPLANE_SLAVE_ADDRESS, STS_I2C, &data, 1);
         printf("%d\n", data);
-    } else if (!strncmp(SEN, buf, 3)) {
-        sen(BACKPLANE_SLAVE_ADDRESS);
+    
+    /***** Debug commands *****/
     } else if (!strncmp(DEV, buf, 3)) {
         dev_addr = atoi(&buf[4]);
     } else if (!strncmp(REG, buf, 3)) {
@@ -219,22 +247,12 @@ void extension_handler(uint8_t *buf) {
     } else if (!strncmp(RED, buf, 3)) {
         i2c_read(dev_addr, reg_addr, &data, 1);
         printf("%d\n", data);
-    } else if (!strncmp(SEN, buf, 3)) {
-        status = i2c_read(dev_addr, SEN_I2C, &data, 1);
-        if (status == 0) printf("type: %d\n", data);
-        else printf("status: %d\n", status);
-        status = i2c_read(dev_addr, SEN_I2C, &data, 1);
-        if (status == 0) printf("length: %d\n", data);
-        else printf("status: %d\n", status);
-        status = i2c_read(dev_addr, SEN_I2C, &read_buf[0], data);
-        if (status == 0) {
-            for (i=0; i<data; i++) printf("value[%d]: %d\n", i, read_buf[i]);        
-        } else {
-            printf("status: %d\n", status);
-        }
     } else if (!strncmp(WRT, buf, 3)) {
         data = atoi(&buf[4]);
         i2c_write(dev_addr, reg_addr, data);
+    } else if (!strncmp(SEN, buf, 3)) {
+        sen(BACKPLANE_SLAVE_ADDRESS);
+    /******/
     }
 }
 
@@ -253,7 +271,7 @@ void main(void)
     
     EUSART_Initialize();
 
-    PROTOCOL_Initialize(DEVICE_ID, start_handler, stop_handler, set_handler);
+    PROTOCOL_Initialize(DEVICE_ID, NULL, NULL, set_handler);
     PROTOCOL_Set_Func(loop_func);
     PROTOCOL_Set_Extension_Handler(extension_handler);
     PROTOCOL_Loop();
